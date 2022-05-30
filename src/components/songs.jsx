2160196e3c57dd84190890e238342a235ffc6dfe
Ale Gómez <gom20347@uvg.edu.gt>
2022-05-29 20:48:54 -0600

    fixed first row on hover

--- src/components/songs.jsx
+++ src/components/songs.jsx
@@ -7,39 +7,45 @@ const Tables = () => (
     <h3 className="title12">HOT SONGS</h3>
     <div className="tableContainer">
       <table className="table">
-        <tr>
-          <td className="tableItem">Los Mesoneros - &quot;Últimas Palabras&quot;</td>
-        </tr>
-        <tr>
-          <td className="tableItem">Los Mesoneros - &quot;Pangea&quot;</td>
-        </tr>
-        <tr>
-          <td className="tableItem">Los Mesoneros - &quot;Solo&quot;</td>
-        </tr>
-        <tr>
-          <td className="tableItem">Los Mesoneros - &quot;Indeleble&quot;</td>
-        </tr>
-        <tr>
-          <td className="tableItem">Los Mesoneros - &quot;Prefiero No Saber&quot;</td>
-        </tr>
+        <th>‎</th>
+        <tbody>
+          <tr>
+            <td className="tableItem">Los Mesoneros - &quot;Últimas Palabras&quot;</td>
+          </tr>
+          <tr>
+            <td className="tableItem">Los Mesoneros - &quot;Pangea&quot;</td>
+          </tr>
+          <tr>
+            <td className="tableItem">Los Mesoneros - &quot;Solo&quot;</td>
+          </tr>
+          <tr>
+            <td className="tableItem">Los Mesoneros - &quot;Indeleble&quot;</td>
+          </tr>
+          <tr>
+            <td className="tableItem">Los Mesoneros - &quot;Prefiero No Saber&quot;</td>
+          </tr>
+        </tbody>
       </table>
 
       <table>
-        <tr>
-          <td className="tableItem">Bad Bunny - &quot;Ye ye ye&quot;</td>
-        </tr>
-        <tr>
-          <td className="tableItem">Bad Bunny - &quot;Neverita&quot;</td>
-        </tr>
-        <tr>
-          <td className="tableItem">Bad Bunny - &quot;Yo No Soy Celoso&quot;</td>
-        </tr>
-        <tr>
-          <td className="tableItem">Bad Bunny - &quot;Callaita&quot;</td>
-        </tr>
-        <tr>
-          <td className="tableItem">Bad Bunny - &quot;Yonaguni&quot;</td>
-        </tr>
+        <th>‎</th>
+        <tbody>
+          <tr>
+            <td className="tableItem">Bad Bunny - &quot;Ye ye ye&quot;</td>
+          </tr>
+          <tr>
+            <td className="tableItem">Bad Bunny - &quot;Neverita&quot;</td>
+          </tr>
+          <tr>
+            <td className="tableItem">Bad Bunny - &quot;Yo No Soy Celoso&quot;</td>
+          </tr>
+          <tr>
+            <td className="tableItem">Bad Bunny - &quot;Callaita&quot;</td>
+          </tr>
+          <tr>
+            <td className="tableItem">Bad Bunny - &quot;Yonaguni&quot;</td>
+          </tr>
+        </tbody>
       </table>
     </div>
   </div>
